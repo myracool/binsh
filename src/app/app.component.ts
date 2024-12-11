563ba7a69a4ac207109cb1a085523cf4a404c79a
m.pascal <mary.emilie@outlook.fr>
2024-12-11 16:11:56 +0100

    close menu when click anywhere

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {StartComponent} from './start/start.component';
 import {PaintComponent} from './paint/paint.component';
@@ -7,7 +7,7 @@ import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, StartComponent, MenuComponent, PaintComponent ],
+  imports: [RouterOutlet, CommonModule, StartComponent, MenuComponent, PaintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,6 +15,13 @@ export class AppComponent {
   title = 'binsh';
   menuIsShown: boolean = false;
   paintIsShown: boolean = false;
+
+  @HostListener('document:click', ['$event', '$event.target'])
+  documentClick(event: MouseEvent, targetElement: HTMLElement) {
+    if (targetElement.className !== "start-button") {
+      this.menuIsShown = false;
+    }
+  }
 
   showMenu() {
     this.menuIsShown = !this.menuIsShown;
